ed04a9409b0002f5905778df7fadc9c7f89e82da
Travis LaDuke <travisladuke@gmail.com>
2021-11-22 14:55:37 -0800

    copy a bunch of promethues from the haproxy repo

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 
+	// "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
+	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
+	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/laduke/zerotier-one_exporter/internal/authtokenservice"
 	"github.com/laduke/zerotier-one_exporter/internal/promexporter"
 	"github.com/laduke/zerotier-one_exporter/internal/ztoneservice"
 )
 
-// TODO make a config service, configure http port # and authtoken and flag to collect addresses or not
+// TODO make a config service, configure authtoken and flag to collect addresses or not
 
 func main() {
+	var (
+		webConfig     = webflag.AddFlags(kingpin.CommandLine)
+		listenAddress = kingpin.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":19993").String()
+		metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+	)
+
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("zerotier_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
+
 	token := authtoken.Guess()
 	oneService := ztone.New(token)
 
@@ -22,8 +43,27 @@ func main() {
 	exporter := promexporter.New(oneService)
 	reg.MustRegister(exporter)
 
-	fmt.Println("starting")
+	level.Info(logger).Log("msg", "Starting zerotier-one_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "context", version.BuildContext())
 
-	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
+	level.Info(logger).Log("msg", "Listening on address", "address", *listenAddress)
+	http.Handle(*metricsPath, promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+             <head><title>ZeroTier Exporter</title></head>
+             <body>
+             <h1>ZeroTier Exporter</h1>
+             <p><a href='` + *metricsPath + `'>Metrics</a></p>
+             </body>
+             </html>`))
+	})
+
+	srv := &http.Server{Addr: *listenAddress}
+
 	http.ListenAndServe(":1971", nil)
+	if err := web.ListenAndServe(srv, *webConfig, logger); err != nil {
+		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/laduke/zerotier-one_exporter
 go 1.16
 
 require (
-	github.com/erikh/go-ztone v0.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/erikh/go-ztone v0.1.0
+	github.com/go-kit/kit v0.9.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/prometheus/client_golang v1.11.0
+	github.com/prometheus/common v0.30.0
+	github.com/prometheus/exporter-toolkit v0.7.0
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
